5b375602a5d12b719ca422d8bcc37c323819d108
Arnav Chahal <122754942+arvchahal@users.noreply.github.com>
2024-06-26 21:41:12 -0500

    Update settings.py

--- apiFolder/settings.py
+++ apiFolder/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-up_$-gtubv==d^bszzwlpbam39yr8iuyh7ma8wmqaoed5t4)jq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.3.15.206.122']
 #['firsttim.azurewebsites.net/','127.0.0.1']
